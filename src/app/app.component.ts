cbcbbeea2931b7be18eb751dc02da290569ca153
grmns <german.sandoval@tecsup.edu.pe>
2024-05-20 11:17:38 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Lab11';
+  title = 'Interpolacion';
+  valor = 'German Sandoval Palacios'
+  tipo = 'password'
+  img = 'assets/angular.png'
+  enlace = 'https://elcomercio.pe'
+  clase01 = 'azul'
+  clase02 = 'rojo'
+
+  onClick() {
+    alert('Utilizando Eventos')
+  }
+
+  nombre = ''
 }
